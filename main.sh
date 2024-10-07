48b6109da167fd64219df359f74ef4ccc89ad9d6
7aGiven <fredrickareichert@gmail.com>
2024-10-08 01:46:07 +0800

    t2

--- main.sh
+++ main.sh
@@ -4,21 +4,23 @@ then
 	echo "No update"
 	exit
 fi
-wget -nv -O Phigros.apk `cat url.txt`
+git config --global user.name '7aGiven'
+git config --global user.email 'a@gmail.com'
+git clone https://$1@github.com/7aGiven/PhigrosAction/
+cd PhigrosAction
+wget -nv -O Phigros.apk `cat ../url.txt`
 java -jar PhigrosMetadata-1.2.jar Phigros.apk
 dotnet Il2CppDumper.dll libil2cpp.so global-metadata.dat .
 dotnet DummyDllToPythonTemplate.dll -a DummyDll/Assembly-CSharp.dll -i DummyDll/Il2CppDummyDll.dll -p h.py
+
+cd ..
 git clone https://$1@github.com/7aGiven/Phigros_Resource/
 cd Phigros_Resource
-mv ../h.py .
-ls -l
-git config --global user.name '7aGiven'
-git config --global user.email 'a@gmail.com'
-git config --global -l
-echo 0
-git add .
-echo 1
+mv ../PhigrosAction/h.py .
 git commit -m "Github Action"
-echo 2
 git push
+
+cd ../PhigrosAction
 echo $version > version_code.txt
+git commit -m "Github Action"
+git push
