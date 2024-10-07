e232c97d3bc0ca10df05cc6e881f9c304cfff0b1
7aGiven <fredrickareichert@gmail.com>
2024-10-08 02:33:18 +0800

    a3

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ then
 	echo "No update"
 	exit
 fi
-wget -nv -O Phigros.apk `cat ../url.txt`
+wget -nv -O Phigros.apk `cat url.txt`
 java -jar PhigrosMetadata-1.2.jar Phigros.apk
 dotnet Il2CppDumper.dll libil2cpp.so global-metadata.dat .
 dotnet DummyDllToPythonTemplate.dll -a DummyDll/Assembly-CSharp.dll -i DummyDll/Il2CppDummyDll.dll -p h.py
