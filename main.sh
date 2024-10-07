edef15f800e5cac29fd4bb93da418cb67868656f
7aGiven <fredrickareichert@gmail.com>
2024-10-08 01:59:07 +0800

    t6

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ then
 fi
 git config --global user.name '7aGiven'
 git config --global user.email 'a@gmail.com'
-git clone https://$1@github.com/7aGiven/PhigrosAction/
+git clone https://$1@github.com/7aGiven/PhigrosAction
 cd PhigrosAction
 wget -nv -O Phigros.apk `cat ../url.txt`
 java -jar PhigrosMetadata-1.2.jar Phigros.apk
@@ -14,13 +14,13 @@ dotnet Il2CppDumper.dll libil2cpp.so global-metadata.dat .
 dotnet DummyDllToPythonTemplate.dll -a DummyDll/Assembly-CSharp.dll -i DummyDll/Il2CppDummyDll.dll -p h.py
 
 cd ..
-git clone https://$1@github.com/7aGiven/Phigros_Resource/
+git clone https://$1@github.com/7aGiven/Phigros_Resource
 cd Phigros_Resource
 mv ../PhigrosAction/h.py .
-git commit -m "Github Action"
+git commit -a -m "Github Action"
 git push
 
 cd ../PhigrosAction
 echo $version > version_code.txt
-git commit -m "Github Action"
+git commit -a -m "Github Action"
 git push
