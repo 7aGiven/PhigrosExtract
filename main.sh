41d78929ba568a4a65a2b4deec219690842ddf18
7aGiven <fredrickareichert@gmail.com>
2024-10-19 12:58:11 +0800

    version

--- main.sh
+++ main.sh
@@ -14,7 +14,7 @@ dotnet Il2CppDumper.dll libil2cpp.so global-metadata.dat .
 dotnet DummyDllToPythonTemplate.dll -a DummyDll/Assembly-CSharp.dll -i DummyDll/Il2CppDummyDll.dll -p Phigros_Resource/h.py
 
 cd Phigros_Resource
-git commit -am "'$version'" && git push
+git commit -am "$version" && git push
 git clone --no-checkout --single-branch -b info https://$1@github.com/7aGiven/Phigros_Resource info
 git clone --no-checkout --single-branch -b avatar https://$1@github.com/7aGiven/Phigros_Resource avatar
 git clone --no-checkout --single-branch -b illustration https://$1@github.com/7aGiven/Phigros_Resource illustration
@@ -25,29 +25,29 @@ python3 resource.py ../Phigros.apk
 
 cd info
 git add .
-git commit -m "'$version'" && git push
+git commit -m "$version" && git push
 cd ..
 
 cd avatar
 git add .
-git commit -m "'$version'" && git push
+git commit -m "$version" && git push
 cd ..
 
 cd illustration
 git add .
-git commit -m "'$version'" && git push
+git commit -m "$version" && git push
 cd ..
 
 cd illustrationBlur
 git add .
-git commit -m "'$version'" && git push
+git commit -m "$version" && git push
 cd ..
 
 cd illustrationLowRes
 git add .
-git commit -m "'$version'" && git push
+git commit -m "$version" && git push
 cd ..
 
 cd ..
 echo $version > version.txt
-git commit -am "'$version'" && git push
+git commit -am "$version" && git push
