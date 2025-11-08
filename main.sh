59038cff9962aaa9f46f278de1474556ce89605f
7aGiven <77519196+7aGiven@users.noreply.github.com>
2025-11-08 23:31:00 +0800

    Refactor main.sh for Phigros resource handling
    Updated commands for processing Phigros.apk and adjusted paths for Il2CppDumper.

--- main.sh
+++ main.sh
@@ -11,11 +11,9 @@ pip install fsb5
 git clone --single-branch -b master --depth 1 https://$1@github.com/7aGiven/Phigros_Resource/
 wget -nv -O Phigros.apk `cat url.txt`
 #java -jar PhigrosMetadata-1.2.jar Phigros.apk
-ls
+# dotnet Il2CppDumper.dll libil2cpp.so global-metadata.dat .
 unzip Phigros.apk assets/bin/Data/Managed/Metadata/global-metadata.dat
-ls
-dotnet Il2CppDumper.dll libil2cpp.so global-metadata.dat .
-ls
+dotnet Il2CppDumper.dll libil2cpp.so assets/bin/Data/Managed/Metadata/global-metadata.dat .
 dotnet TypeTreeGeneratorCLI.dll -p DummyDll/ -a Assembly-CSharp.dll -v 2019.4.31f1c1 -c GameInformation -c GetCollectionControl -c TipsProvider -d json_min -o Phigros_Resource/typetree.json
 
 cd Phigros_Resource
