c73ced100ad536ecbebafcf252a4c26166e038e3
7aGiven <77519196+7aGiven@users.noreply.github.com>
2025-11-08 22:32:59 +0800

    Comment out Java execution for PhigrosMetadata
    Comment out the Java command to run PhigrosMetadata.

--- main.sh
+++ main.sh
@@ -10,7 +10,8 @@ pip install UnityPy~=1.10.0
 pip install fsb5
 git clone --single-branch -b master https://$1@github.com/7aGiven/Phigros_Resource/
 wget -nv -O Phigros.apk `cat url.txt`
-java -jar PhigrosMetadata-1.2.jar Phigros.apk
+#java -jar PhigrosMetadata-1.2.jar Phigros.apk
+unzip Phigros.apk /assets/bin/Data/Managed/Metadata/global-metadata.dat
 dotnet Il2CppDumper.dll libil2cpp.so global-metadata.dat .
 dotnet TypeTreeGeneratorCLI.dll -p DummyDll/ -a Assembly-CSharp.dll -v 2019.4.31f1c1 -c GameInformation -c GetCollectionControl -c TipsProvider -d json_min -o Phigros_Resource/typetree.json
 
